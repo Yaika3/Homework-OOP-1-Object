57ef076d535bbb507ad94a17668efeadd4587279
max <kabinidi@mail.ru>
2022-12-13 13:22:10 +0300

    ДЗ объекты Human.ООП

--- src/Human.java
+++ src/Human.java
@@ -4,13 +4,31 @@ public class Human {
     int yearOfBirth;
     String town;
     String jobTitle;
+
     Human(String name, int yearOfBirth, String town,String jobTitle){
         this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.town = town;
-        this.jobTitle = jobTitle;
+        if ( this.name == null )
+        { this.name = "Информация не указана";
+            }
+        else this.name = name;
 
-    }
+
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth =yearOfBirth;}
+        else {
+            this.yearOfBirth=Math.abs(yearOfBirth);
+        }
+        this.town=town;
+        if ( this.town == null )
+        { this.town = "Информация не указана";
+        }
+        else this.town = town;
+
+        this.jobTitle = jobTitle;
+        if ( this.jobTitle == null )
+        { this.jobTitle = "Информация не указана";
+        }
+        else this.jobTitle = jobTitle;}
 
     @Override
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human максим = new Human("Максим",1988, "Минск","бренд-менеджером");
-        Human аня = new Human("Аня", 1993 , " Москва","методистом образовательных программ");
+        Human максим = new Human(" Максим",-1988, "Минск","бренд-менеджером");
+        Human аня = new Human("Аня", -1993 , " Москва","методистом образовательных программ");
         Human катя = new Human("Катя", 1992 , " Калининград","продакт-менеджером");
         Human артем = new Human("Артем", 1995 , " Москва","директором по развитию бизнеса");
+        Human nulL = new Human(null,-1990,null,null);
         System.out.println(максим.toString());
         System.out.println(аня.toString());
         System.out.println(катя.toString());
         System.out.println(артем.toString());
-
+        System.out.println(nulL.toString());
 
     }
 }
