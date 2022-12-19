ff0b6e61d529c68c8f8bcbbdb68f319d26dd8d11
max <kabinidi@mail.ru>
2022-12-19 11:26:50 +0300

    ДЗ объекты Human.ООП

--- src/Human.java
+++ src/Human.java
@@ -1,9 +1,33 @@
 public class Human {
 
     String name;
-    int yearOfBirth;
-    String town;
+    private int yearOfBirth;
+    private String town;
     String jobTitle;
+
+    public int getYearOfBirth(){
+        return yearOfBirth;
+    }
+    public void seYearOfBrith(int yearOfBirth){
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth =yearOfBirth;}
+        else {
+            this.yearOfBirth=Math.abs(yearOfBirth);
+        }
+    }
+    // Спросил у ребят,кто как сделал...по их ответам,никак.   Нельзя сравнить int по null, Empty. (можно в строку переделать,Но тогда на <0, не проверить)
+    //Так же на empty не проверить int , так как если не заполнить поле то все ломается,а в кавычках число не пишется)
+    public String getTown(){
+        return town;
+    }
+    public void setTown( String town){
+        this.town=town;
+        if ( this.town == null||town.isEmpty())
+        { this.town = "Информация не указана";
+        }
+        else this.town = town;
+    }
+
 
     Human(String name, int yearOfBirth, String town,String jobTitle){
         this.name = name;
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,17 @@ public class Main {
         Human катя = new Human("Катя", 1992 , " Калининград","продакт-менеджером");
         Human артем = new Human("Артем", 1995 , " Москва","директором по развитию бизнеса");
         Human nulL = new Human(null,-1990,"",null);
+        Human владимир = new Human("Владимир",2001,"Казань","не работает");
         System.out.println(максим.toString());
         System.out.println(аня.toString());
         System.out.println(катя.toString());
         System.out.println(артем.toString());
         System.out.println(nulL.toString());
+        System.out.println(максим.getYearOfBirth());
+        System.out.println(максим.getTown());
+        System.out.println(владимир.toString());
+        System.out.println(владимир.getTown());
+        System.out.println(владимир.getYearOfBirth());
 
     }
 }
